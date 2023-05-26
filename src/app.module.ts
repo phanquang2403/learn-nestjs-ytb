99a9dbbbbd382b9f93b7ee0addf7f71d2f09a412
phanquang <phandangquang2403@gmail.com>
2023-05-26 17:08:02 +0700

    config db

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { TodoModule } from './todo/todo.module';
+import config from './config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule],
+  imports: [TypeOrmModule.forRoot(config), UsersModule, TodoModule],
   controllers: [AppController],
   providers: [AppService],
 })
